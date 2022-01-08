5adbaf0be8fc9db9c18212c96fe5b91e01e6acbe
Ryan Olea <ryanaolea@gmail.com>
2022-01-08 14:24:32 -0800

    new files for ratings testing

--- client/src/components/RatingsComponents/TestingRatings/sampleData.js
+++ client/src/components/RatingsComponents/TestingRatings/sampleData.js
@@ -409,5 +409,96 @@ const sampleData = {
         }
       }
     }
-  ]
+  ],
+  reviews: {
+    'product': '40344',
+    'page': 0,
+    'count': 5,
+    'results': [
+      {
+        'review_id': 1115955,
+        'rating': 5,
+        'summary': 'We come in peace',
+        'recommend': true,
+        'response': null,
+        'body': 'These earth pants are most comfortable. I enjoy wearing them on my long space journeys.',
+        'date': '2022-01-07T00:00:00.000Z',
+        'reviewer_name': 'Allen',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1115951,
+        'rating': 4,
+        'summary': '',
+        'recommend': true,
+        'response': null,
+        'body': 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf',
+        'date': '2022-01-07T00:00:00.000Z',
+        'reviewer_name': 'guy',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1115727,
+        'rating': 4,
+        'summary': 'this is a summaruuuuuu',
+        'recommend': true,
+        'response': null,
+        'body': 'fds dsj fjdsjf jdskfj dslkjfkds jfjdslkjfkldsjfldsjlfjdslj',
+        'date': '2022-01-05T00:00:00.000Z',
+        'reviewer_name': 'mynameisRyan',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1115703,
+        'rating': 3,
+        'summary': 'This is a good product Buy it please This is a good product Buy it, please',
+        'recommend': true,
+        'response': null,
+        'body': 'This is a good product Buy it please THere is a min number of charsThis is a good product Buy it please THere is a min number of chars',
+        'date': '2022-01-05T00:00:00.000Z',
+        'reviewer_name': 'notryano',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1115792,
+        'rating': 2,
+        'summary': 'very ok',
+        'recommend': false,
+        'response': null,
+        'body': 'it was aite ',
+        'date': '2022-01-05T00:00:00.000Z',
+        'reviewer_name': 'alissa',
+        'helpfulness': 0,
+        'photos': [
+          {
+            'id': 2140910,
+            'url': 'https://res.cloudinary.com/dkx7ghwza/image/upload/v1641412532/kypisitnjusc2ypn05en.png'
+          },
+          {
+            'id': 2140911,
+            'url': 'https://res.cloudinary.com/dkx7ghwza/image/upload/v1641412508/saylqtltgfzn0aktez3y.png'
+          },
+          {
+            'id': 2140909,
+            'url': 'https://res.cloudinary.com/dkx7ghwza/image/upload/v1641412517/gn1mdw72qeunwtidzpsg.png'
+          },
+          {
+            'id': 2140912,
+            'url': 'https://res.cloudinary.com/dkx7ghwza/image/upload/v1641412524/xqqbdzllhzjkx0sjdhr6.png'
+          },
+          {
+            'id': 2140913,
+            'url': 'https://res.cloudinary.com/dkx7ghwza/image/upload/v1641412540/wc8fwaxrbelxvx7tdarj.png'
+          }
+        ]
+      }
+    ]
+  }
 };
+
+export default sampleData;
+export const { reviews, metadata } = sampleData;
